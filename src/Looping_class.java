7164491c658413695b59f73c04ce47d2aa741d41
yashwanthkumarrr <kyashwanth737@gmail.com>
2022-11-28 17:10:32 +0530

    git commit -m "update"

--- src/Looping_class.java
+++ src/Looping_class.java
@@ -3,17 +3,48 @@ public class Looping_class {
 
         Looping_class obj = new Looping_class();
        // obj.divisor_of_given_num(21);
-        obj.count_divisor(21);
+        //obj.count_divisor(21);
+        obj.prime_num(12);
 
 
         }
+
+    private void prime_num(int no) {
+        int i=2;
+
+        boolean re = true;
+        while(i<=no/2){
+            if(no%i==0){
+                System.out.println("it is a not prime");
+                re=false;
+                break;
+            }
+
+            i++;
+        }
+        if(re==true){
+            System.out.println("it is a prime number");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 
     private void count_divisor(int no) {
         int i=1;
         int count=0;
         while(i<=no){
             if(no%i==0){
-                //System.out.println(i);
+
                 count=count+1;
             }
             i++;
